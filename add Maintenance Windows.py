0baf5b93ec873932f46546bed5db0eaec0c65b79
Andreas <abuis@splunk.com>
2020-08-20 10:11:19 +0000

    df

--- add Maintenance Windows.py
+++ add Maintenance Windows.py
@@ -18,44 +18,25 @@ def add_maintenance_window_1(action=None, success=None, container=None, results=
     #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
     
     # collect data for 'add_maintenance_window_1' call
-    container_data = phantom.collect2(container=container, datapath=['artifact:*.cef.entity_key', 'artifact:*.id'])
-    formatted_data_1 = phantom.get_format_data(name='format_1')
+    custom_function_results_data_1 = phantom.collect2(container=container, datapath=['cf_community_list_deduplicate_1:custom_function_result.data.*.item'], action_results=results)
 
     parameters = []
     
     # build parameters list for 'add_maintenance_window_1' call
-    for container_item in container_data:
-        if container_item[0]:
+    for custom_function_results_item_1 in custom_function_results_data_1:
+        if custom_function_results_item_1[0]:
             parameters.append({
-                'title': formatted_data_1,
+                'title': "Bluber",
                 'comment': "Phantom hast started Maintenance Windows",
                 'end_time': "",
-                'object_ids': container_item[0],
+                'object_ids': custom_function_results_item_1[0],
                 'start_time': "",
                 'object_type': "entity",
                 'relative_end_time': 300,
                 'relative_start_time': "",
-                # context (artifact id) is added to associate results with the artifact
-                'context': {'artifact_id': container_item[1]},
             })
 
     phantom.act(action="add maintenance window", parameters=parameters, assets=['splunk itsi'], callback=pin_1, name="add_maintenance_window_1")
-
-    return
-
-def format_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug('format_1() called')
-    
-    template = """Maintenance Windows for entity {0}"""
-
-    # parameter list for template variable replacement
-    parameters = [
-        "cf_community_list_deduplicate_1:custom_function_result.data.*.item",
-    ]
-
-    phantom.format(container=container, template=template, parameters=parameters, name="format_1")
-
-    add_maintenance_window_1(container=container)
 
     return
 
@@ -94,26 +75,33 @@ def cf_community_list_deduplicate_1(action=None, success=None, container=None, r
     ################################################################################    
 
     # call custom function "community/list_deduplicate", returns the custom_function_run_id
-    phantom.custom_function(custom_function='community/list_deduplicate', parameters=parameters, name='cf_community_list_deduplicate_1', callback=cf_community_list_deduplicate_1_callback)
+    phantom.custom_function(custom_function='community/list_deduplicate', parameters=parameters, name='cf_community_list_deduplicate_1', callback=format_2)
 
     return
 
-def cf_community_list_deduplicate_1_callback(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None):
-    phantom.debug('cf_community_list_deduplicate_1_callback() called')
+def format_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug('format_2() called')
     
-    format_1(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function)
-    add_comment_2(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function)
+    template = """{0}"""
+
+    # parameter list for template variable replacement
+    parameters = [
+        "cf_community_list_deduplicate_1:custom_function_result.data.*.item",
+    ]
+
+    phantom.format(container=container, template=template, parameters=parameters, name="format_2")
+
+    add_maintenance_window_1(container=container)
+    add_comment_3(container=container)
 
     return
 
-def add_comment_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug('add_comment_2() called')
+def add_comment_3(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug('add_comment_3() called')
 
-    custom_function_results_data_1 = phantom.collect2(container=container, datapath=['cf_community_list_deduplicate_1:custom_function_result.data.*.item'], action_results=results)
+    formatted_data_1 = phantom.get_format_data(name='format_2')
 
-    custom_function_results_item_1_0 = [item[0] for item in custom_function_results_data_1]
-
-    phantom.comment(container=container, comment=custom_function_results_item_1_0)
+    phantom.comment(container=container, comment=formatted_data_1)
 
     return
 
